5fb66244656feff5dd7398a2fe5c463d43b34842
sdoaolo <09jieun01@naver.com>
2023-03-07 14:32:14 +0900

    [추가] first commit (23.03.07 강지은)

---
+++ backend/src/test/java/com/mungfluencer/MungfluencerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mungfluencer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungfluencerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
