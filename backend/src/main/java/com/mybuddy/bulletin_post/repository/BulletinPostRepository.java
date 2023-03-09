b10d3bde04fa46a24998781873b98493440da774
Daw-Jeong <lvdst06@gmail.com>
2023-03-10 02:29:16 +0900

    [추가] BulletinPost CRUD (2023.03.10 정다운)

--- backend/src/main/java/com/mybuddy/bulletin_post/repository/BulletinPostRepository.java
+++ backend/src/main/java/com/mybuddy/bulletin_post/repository/BulletinPostRepository.java
@@ -1,4 +1,9 @@
 package com.mybuddy.bulletin_post.repository;
 
-public interface BulletinPostRepository {
+import com.mybuddy.bulletin_post.entity.BulletinPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BulletinPostRepository extends JpaRepository<BulletinPost, Long> {
+
+    //해당 장소 id를 파라미터로 받는 findFirstPost()메서드가 있다고 가정하고 해당 사진의 URL을 반환하는걸 작성
 }
