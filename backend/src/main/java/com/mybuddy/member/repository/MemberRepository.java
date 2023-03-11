1cfe6be79cc94db2323207ca18012e23355f41c5
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-03-11 21:50:37 +0900

    [수정] findExistMemberById 쿼리 및 로직 수정

--- backend/src/main/java/com/mybuddy/member/repository/MemberRepository.java
+++ backend/src/main/java/com/mybuddy/member/repository/MemberRepository.java
@@ -1,10 +1,15 @@
 package com.mybuddy.member.repository;
 
+
 import com.mybuddy.member.entity.Member;
+import com.mybuddy.member.entity.Member.MemberStatus;
+
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByMemberIdAndMemberStatusIs(Long memberId, MemberStatus memberStatus);
 }
