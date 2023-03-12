f93ce8c754b228e01dfe345a77b2194031f77e9c
Jung Su <wjdwjdtn92@naver.com>
2023-03-12 19:04:04 +0900

    [수정] box-shadow 형식으로 변경 및 컴포넌트 병합

--- frontend/src/components/UI/Card/Card.jsx
+++ frontend/src/components/UI/Card/Card.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const StyledCard = styled.div`
   border: 1.5px solid var(--black);
   border-radius: ${({ borderRadius }) => borderRadius};
   background: var(--white);
+
+  ${({ isHoverTranslate }) =>
+    isHoverTranslate &&
+    css`
+      &:hover {
+        transform: translate(-0.5rem, -0.625rem);
+        box-shadow: 8px 10px 0 0 var(--color-dark-0);
+      }
+    `}
+
+  ${({ isTranslate }) =>
+    isTranslate &&
+    css`
+      transform: translate(-0.5rem, -0.625rem);
+      box-shadow: 8px 10px 0 0 var(--color-dark-0);
+    `}
 `;
 
-function Card({ borderRadius = '10px', children, ...props }) {
+function Card({
+  borderRadius = '10px',
+  isHoverTranslate = false,
+  isTranslate = false,
+  children,
+  ...props
+}) {
   return (
-    <StyledCard borderRadius={borderRadius} {...props}>
+    <StyledCard
+      isHoverTranslate={isHoverTranslate}
+      isTranslate={isTranslate}
+      borderRadius={borderRadius}
+      {...props}
+    >
       {children}
     </StyledCard>
   );
