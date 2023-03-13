a5f65088a967c9161f80f785f070267e74d1091b
Daw-Jeong <lvdst06@gmail.com>
2023-03-14 00:32:45 +0900

    [수정] bulletinPost 주석 해제, BulletinPostService 수정 (2023.03.14 정다운)

--- backend/src/main/java/com/mybuddy/bulletin_post/service/BulletinPostService.java
+++ backend/src/main/java/com/mybuddy/bulletin_post/service/BulletinPostService.java
@@ -3,12 +3,19 @@ package com.mybuddy.bulletin_post.service;
 import com.mybuddy.amenity.entity.Amenity;
 import com.mybuddy.bulletin_post.entity.BulletinPost;
 import com.mybuddy.bulletin_post.repository.BulletinPostRepository;
+import com.mybuddy.comment.entity.Comment;
+import com.mybuddy.comment.service.CommentService;
+import com.mybuddy.global.storage.StorageService;
+import com.mybuddy.global.utils.CustomBeanUtils;
+import com.mybuddy.member.entity.Member;
+import com.mybuddy.member.service.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,24 +24,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class BulletinPostService {
 
-
-//        관련 메서드, 로직 만들기 이걸 service단에다..? response dto단에다? controller단에다?
-//        private long likeCount = bulletinPostService.getLikeCount(bulletinPostId);
-//        private int likeChosen = bullentinPostService.getLikeChosen(bulletinPostId, memberId);
-
-
     private final BulletinPostRepository bulletinPostRepository;
+    private final MemberServiceImpl memberService;
+    private final CustomBeanUtils<BulletinPost> customBeanUtils;
+    private final StorageService storageService;
+    private final CommentService commentService;
 
-    public BulletinPost createBulletinPost(BulletinPost bulletinPost, Amenity amenity) {
+    public BulletinPost createBulletinPost(BulletinPost bulletinPost, Amenity amenity, MultipartFile photoImage) {
 
-//        multipartFile을 S3에 저장하고 해당 photoUrl 가져오는 service 메서드
+//        multipartFile을 저장소에 저장하고 해당 photoUrl 가져오는 service 메서드
+        storageService.storeImage(photoImage);
+        bulletinPost.setPhotoUrl(
+                        storageService.getPath() + "/" + photoImage.getOriginalFilename());
 
-//        findVerifiedMember 가져오기
+//        controller에서 memberId 찾아서 먼저 저장해야하려나
 //        Member member = bulletinPost.getMember();
-//        memberService.findVerifiedMember(member.getEmail());
-
-        //임시
-        bulletinPost.setPhotoUrl("/photoUrl/blah/blah/blah");
+//        Member foundMember = memberService.findExistMemberById(member.getMemberId());
 
         //amenity 연결
         bulletinPost.setAmenity(amenity);
@@ -44,19 +49,27 @@ public class BulletinPostService {
         return createdBulletinPost;
     }
 
-    public BulletinPost updateBulletinPost(BulletinPost updateBulletinPost) {
+    public BulletinPost updateBulletinPost(BulletinPost updateBulletinPost, Amenity amenity, MultipartFile photoImage) {
 
-        //findExistMemberById(Long memberId) 가져오기
+        //findExistMemberById(Long memberId) 가져오기? 로그인 사용자 확인하기?
+
 
         BulletinPost bulletinPost = findVerifiedBulletinPost(updateBulletinPost.getBulletinPostId());
 
-        Optional.ofNullable(updateBulletinPost.getPostContent())
-                .ifPresent(content -> bulletinPost.setPostContent(content));
+        BulletinPost tempBulletinPost = customBeanUtils.copyNonNullProperties(updateBulletinPost, bulletinPost);
 
-        //임시
-        bulletinPost.setPhotoUrl("/photoUrl/l/b/h");
+        Optional.ofNullable(photoImage)
+                .ifPresent(storageService::storeImage);
+        Optional.ofNullable(photoImage)
+                .ifPresent(image -> bulletinPost.setPhotoUrl(
+                        storageService.getPath() + "/" + image.getOriginalFilename())
+                );
+
+        //amenity 연결
+        bulletinPost.setAmenity(amenity);
 
         BulletinPost updatedBulletinPost = bulletinPostRepository.save(bulletinPost);
+
         return updatedBulletinPost;
     }
 
@@ -64,9 +77,11 @@ public class BulletinPostService {
 
         BulletinPost bulletinPost = findVerifiedBulletinPost(bulletinPostId);
 
-
         return bulletinPost;
     }
+
+
+
 
     public Page<BulletinPost> findBulletinPosts(int page, int size) {
 
@@ -74,16 +89,17 @@ public class BulletinPostService {
         //if 로그인 확인시 //팔로잉 하는 계정들에서.
 
 //        Pageable pageable = PageRequest.of(page, size);
-////        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "likeCount 고쳐서쓰기"));
+//        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "likeCount 고쳐서쓰기"));
 //
-//        Optional<Page<BulletinPost>> optionalPage = bulletinPostRepository.findBy~~(memberId, pageable);
+//        bulletinPostCustomRepositoryImpl에다가 findAllFollowingPostsByMemberId(memberId, (PageRequest.of(page, size)) 메서드
+//        Optional<Page<BulletinPost>> optionalPage = bulletinPostRepository.findAllFollowingPostsByMemberId(memberId, (PageRequest.of(page, size));
 //
 //        Page<BulletinPost> findPage =
 //                optionalPage.orElseThrow(() ->
 //                        new RuntimeException());
 //        return findPage;
 
-        //else 비로그인시 find All 이거 그대로.
+//        else 비로그인시 find All 이거 그대로.
 
         Page<BulletinPost> bulletinPosts = bulletinPostRepository.findAll(PageRequest.of(page, size));
 
@@ -92,23 +108,21 @@ public class BulletinPostService {
 
     public Page<BulletinPost> findBulletinPostsByMemberId(long memberId, int page, int size) {
 
-        //authentication에서
-        //findExistMemberById(Long memberId) 가져오기
+        //findExistMemberById(Long memberId) 가져오기??
 
-//        Pageable pageable = PageRequest.of(page, size);
-////        Pageable pageable = PageRequest.of(page, size);
-//
-//        Optional<Page<BulletinPost>> optionalPage = bulletinPostRepository.findByBulletinByMemberId(memberId, pageable);
-//
-//        Page<BulletinPost> findPage =
-//                optionalPage.orElseThrow(() ->
-//                        new RuntimeException());
-//        return findPage;
+        Pageable pageable = PageRequest.of(page, size);
+
+        Optional<Page<BulletinPost>> optionalPage = bulletinPostRepository.findByMemberMemberId(memberId, pageable);
+
+        Page<BulletinPost> findPage =
+                optionalPage.orElseThrow(() ->
+                        new RuntimeException());
+        return findPage;
 
         //임시
-        Page<BulletinPost> bulletinPosts = bulletinPostRepository.findAll(PageRequest.of(page, size));
-
-        return bulletinPosts;
+//        Page<BulletinPost> bulletinPosts = bulletinPostRepository.findAll(PageRequest.of(page, size));
+//
+//        return bulletinPosts;
     }
 
 
@@ -131,6 +145,34 @@ public class BulletinPostService {
 
         return findBulletinPost;
     }
+
+    public long getCommentCount(long bulletinPostId) {
+
+        //이렇게 할지 아니면 디비에서 comment 갯수를 세오는 쿼리문을 만들어야 할지
+        //쿼리문으로 해야할거 같긴 한데..
+        List<Comment> commentList = commentService.getCommentsByBulletinPostId(bulletinPostId);
+        long commentCount = commentList.size();
+
+        return commentCount;
+    }
+
+//    public long getLikeCount(long bulletinPostId) {
+//
+//        BulletinPost bulletinPost = findVerifiedBulletinPost(bulletinPostId);
+//        List<Like> likeList = bulletinPost.getLikeList();
+//        long likeCount = likeList.size();
+//
+//        return likeCount;
+//    }
+
+//    이렇게 해야할지 아니면 아예 likeservice 에서 findExistLikeByMemberId를 getLikeChosen 메서드명으로 해서 바로 소환해야할지..?
+//    public long getLikeChosen(long bulletinPostId, long memberId) {
+//
+//        //이 메서드 결과 값을 if로 나눠서 0 또는 1로
+//        long OneIfExist = likeService.findExistLikeByMemberId(long bulletinPostId, long memberId);
+//
+//        return OneIfExist;
+//    }
 
 
 }
