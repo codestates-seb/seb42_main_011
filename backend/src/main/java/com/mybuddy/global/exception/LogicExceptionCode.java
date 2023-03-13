3c2d5eff2e0bebb2e524ee2ca58d7319614ba245
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-03-13 20:24:26 +0900

    [추가] 로그인 회원 식별자와 리소스 Owner 식별자 비교 로직 추가

--- backend/src/main/java/com/mybuddy/global/exception/LogicExceptionCode.java
+++ backend/src/main/java/com/mybuddy/global/exception/LogicExceptionCode.java
@@ -7,6 +7,7 @@ public enum LogicExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_ALREADY_EXISTS(409, "Member already exists"),
     NICKNAME_ALREADY_EXISTS(409, "Nickname already exists"),
+    MEMBER_UNAUTHORIZED(401, "Member Unauthorized"),
     BULLETIN_POST_NOT_FOUND(404, "Post not found"),
     COMMENT_NOT_FOUND(404, "Comment not found");
 
