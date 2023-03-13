1000da38e32310deb92bd2c958af29a6bcc84105
Daw-Jeong <lvdst06@gmail.com>
2023-03-13 19:36:48 +0900

    [수정] bulletinPost mapper 수동매핑 완료, like 매핑은 추후 수정 예정  (2023.03.13 정다운)

--- backend/src/main/java/com/mybuddy/bulletin_post/mapper/BulletinPostMapper.java
+++ backend/src/main/java/com/mybuddy/bulletin_post/mapper/BulletinPostMapper.java
@@ -1,15 +1,21 @@
 package com.mybuddy.bulletin_post.mapper;
 
+import com.mybuddy.amenity.entity.Amenity;
 import com.mybuddy.bulletin_post.dto.BulletinPostDto;
 import com.mybuddy.bulletin_post.entity.BulletinPost;
+import com.mybuddy.comment.dto.CommentResponseDto;
+import com.mybuddy.member.entity.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BulletinPostMapper {
+
+    //    amenity를 따로 set 해주니까 필요 없을수도..? 매핑 오류가 안난다면
 //    @Mapping(source = "addressId", target = "amenity.addressId")
 //    @Mapping(source = "amenityName", target = "amenity.amenityName")
 //    @Mapping(source = "address", target = "amenity.address")
@@ -17,6 +23,7 @@ public interface BulletinPostMapper {
 //    @Mapping(source = "latitude", target = "amenity.latitude")
     BulletinPost bulletinPostCreateDtoToBulletinPost(BulletinPostDto.Create requestBody);
 
+    //    amenity를 따로 set 해주니까 필요 없을수도..? 매핑 오류가 안난다면
 //    @Mapping(source = "addressId", target = "amenity.addressId")
 //    @Mapping(source = "amenityName", target = "amenity.amenityName")
 //    @Mapping(source = "address", target = "amenity.address")
@@ -24,39 +31,90 @@ public interface BulletinPostMapper {
 //    @Mapping(source = "latitude", target = "amenity.latitude")
     BulletinPost bulletinPostPatchDtoToBulletinPost(BulletinPostDto.Patch requestBody);
 
-//    @Mapping(source = "member.memberId", target = "memberId")
-//    @Mapping(source = "member.nickname", target = "nickname")
-//    @Mapping(source = "member.dogName", target = "dogName")
-//    @Mapping(source = "member.profileUrl", target = "profileUrl")
-//    @Mapping(source = "amenity.amenityId", target = "amenityId")
-//    @Mapping(source = "amenity.amenityName", target = "amenityName")
-    BulletinPostDto.Response BulletinPostToBulletinPostResponseDto(BulletinPost bulletinPost);
 
-//    수동매핑 로직
-//    default BulletinPostResponseDto questionToBulletinPostResponseDto(BulletinPost question) {
-//
-//        if ( question == null ) {
-//            return null;
-//        }
-//
-//        Member member = question.getMember();
-//        VoteQ voteQ = question.getVoteQ();
-//        long questionId = question.getBulletinPostId();
-//        List<AnswerDto.Response> answers = question.getAnswerList().stream()
-//                .map(answer -> {
-//                    AnswerDto.Response answerResponse = new AnswerDto.Response();
-//                    answerResponse.setAnswerId(answer.getAnswerId());
-//                    answerResponse.setBulletinPostId(questionId);
-//                    answerResponse.setMemberId(answer.getMember().getMemberId());
-//                    answerResponse.setContents(answer.getContents());
-//                    answerResponse.setVoteACount(answer.getVoteA().getVoteACount());
-//                    return answerResponse;
-//                }).collect(Collectors.toList());
-//        BulletinPostResponseDto questionResponseDto = new BulletinPostResponseDto(questionId, question.getBulletinPostTitle(), question.getCreatedAt(), question.getBulletinPostContents(), question.getBulletinPostTrial(), member.getMemberId(), voteQ.getVoteQCount(), answers);
-//        return questionResponseDto;
-//    }
+
+    default BulletinPostDto.Response bulletinPostToBulletinPostResponseDto(BulletinPost bulletinPost) {
+
+        if ( bulletinPost == null ) {
+            return null;
+        }
+
+        Member member = bulletinPost.getMember();
+        Amenity amenity = bulletinPost.getAmenity();
+
+        List<CommentResponseDto> commentLists = bulletinPost.getCommentList().stream()
+                .map(comment -> {
+                    CommentResponseDto commentResponse = new CommentResponseDto(
+                            comment.getCommentContent(),
+                            comment.getMember().getMemberId(),
+                            comment.getMember().getNickname(),
+                            comment.getMember().getDogName()
+                    );
+                    return commentResponse;
+                }
+                ).collect(Collectors.toList());
+
+        //like count, like chosen 추후수정
+//        likeCount = bulletinPostService.getLikeCount(bulletinPostId);
+//        likeChosen = bullentinPostService.getLikeChosen(bulletinPostId, memberId);
+        BulletinPostDto.Response bulletinPostResponseDto = new BulletinPostDto.Response(
+                bulletinPost.getBulletinPostId(),
+                bulletinPost.getPhotoUrl(),
+                bulletinPost.getPostContent(),
+                bulletinPost.getCreatedAt(),
+                member.getMemberId(),
+                member.getNickname(),
+                member.getDogName(),
+                member.getProfileUrl(),
+                amenity.getAmenityId(),
+                amenity.getAmenityName(),
+                1,
+                2,
+                commentLists,
+                commentLists.size()
+        );
+
+        return bulletinPostResponseDto;
+    }
 
     List<BulletinPostDto.Response> bulletinPostsToBulletinPostResponseDtos(List<BulletinPost> bulletinPosts);
+
+    default BulletinPostDto.ResponseForFeed bulletinPostToBulletinPostResponseForFeedDto(BulletinPost bulletinPost) {
+
+        if ( bulletinPost == null ) {
+            return null;
+        }
+
+        Member member = bulletinPost.getMember();
+
+        List<CommentResponseDto> commentLists = bulletinPost.getCommentList().stream()
+                .map(comment -> {
+                    CommentResponseDto commentResponse = new CommentResponseDto(
+                            comment.getCommentContent(),
+                            comment.getMember().getMemberId(),
+                            comment.getMember().getNickname(),
+                            comment.getMember().getDogName()
+                    );
+                    return commentResponse;
+                }
+                ).collect(Collectors.toList());
+
+        BulletinPostDto.ResponseForFeed bulletinPostResponseForFeedDto = new BulletinPostDto.ResponseForFeed(
+                bulletinPost.getBulletinPostId(),
+                bulletinPost.getPhotoUrl(),
+                bulletinPost.getPostContent(),
+                member.getMemberId(),
+                member.getNickname(),
+                member.getDogName(),
+                commentLists,
+                commentLists.size()
+        );
+
+        return bulletinPostResponseForFeedDto;
+    }
+
     List<BulletinPostDto.ResponseForFeed> bulletinPostsToBulletinPostResponseForFeedDtos(List<BulletinPost> bulletinPosts);
 
 }
+
+
