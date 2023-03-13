79c5606cd069dcc420a79db763ca63c344e1fb15
Daw-Jeong <lvdst06@gmail.com>
2023-03-13 16:11:42 +0900

    [수정/추가] BulletinPost repository 수정, CustomRepository,Impl 추가 (2023.03.13 정다운)

--- backend/src/main/java/com/mybuddy/bulletin_post/repository/BulletinPostRepository.java
+++ backend/src/main/java/com/mybuddy/bulletin_post/repository/BulletinPostRepository.java
@@ -1,9 +1,17 @@
 package com.mybuddy.bulletin_post.repository;
 
 import com.mybuddy.bulletin_post.entity.BulletinPost;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BulletinPostRepository extends JpaRepository<BulletinPost, Long> {
+import java.util.Optional;
+
+//@Repository 필수인가?
+public interface BulletinPostRepository extends JpaRepository<BulletinPost, Long>, BulletinPostCustomRepository {
+
+    Optional<Page<BulletinPost>> findByMemberMemberId(long memberId, Pageable pageable);
 
     //해당 장소 id를 파라미터로 받는 findFirstPost()메서드가 있다고 가정하고 해당 사진의 URL을 반환하는걸 작성
 }
