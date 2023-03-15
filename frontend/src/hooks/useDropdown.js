1955aa7bfa71c8ccb14fe4d13bcb0499b69a5655
Jung Su <wjdwjdtn92@naver.com>
2023-03-15 14:37:12 +0900

    [수정] 드롭다운 컴포넌트 분리

---
+++ frontend/src/hooks/useDropdown.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function useDropdown({
+  defaultDlsplayText,
+  onSelect = () => {},
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(defaultDlsplayText);
+
+  const handleDropdownToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionSelect = event => {
+    const { dropdownOption } = event.target.closest('li').dataset;
+
+    if (!dropdownOption) {
+      return;
+    }
+
+    setSelectedOption(dropdownOption);
+    setIsOpen(false);
+    onSelect(dropdownOption);
+  };
+
+  return [isOpen, selectedOption, handleDropdownToggle, handleOptionSelect];
+}
