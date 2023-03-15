fdb7d41956026c0c166f53adb611e13820d12877
JihooKang-KOR <jihoo.kang.kor@gmail.com>
2023-03-15 16:52:09 +0900

    [추가] Member의 Response로 follower/following 정보 추가

--- backend/src/main/java/com/mybuddy/member/mapper/MemberMapper.java
+++ backend/src/main/java/com/mybuddy/member/mapper/MemberMapper.java
@@ -39,13 +39,21 @@ public interface MemberMapper {
                         .build())
                 .collect(Collectors.toList());
 
+        /**
+         * 연관 관계 매핑은 memberId를 중심으로 연결되므로 followerNumber를 count할 경우
+         * 본인을 follow하는 사람들의 수가 아니라 본인이 follower로서 따르는 회원의 수가 된다.
+         * 그러므로 MemberMapper에서는 follower와 followee의 수를 반대로 입력해야 한다.
+         */
+        Long numberOfUserAsFollower = (long) member.getFollowers().size();
+        Long numberOfUserAsFollowee = (long) member.getFollowees().size();
+
         MemberResponseDto memberResponseDto = MemberResponseDto.builder()
                 .nickname(member.getNickname())
                 .dogName(member.getDogName())
                 .dogGender(member.getDogGender())
                 .aboutMe(member.getAboutMe())
-                .followerNumber(null)
-                .followeeNumber(null)
+                .followerNumber(numberOfUserAsFollowee)
+                .followeeNumber(numberOfUserAsFollower)
                 .profileUrl(member.getProfileUrl())
                 .myBulletinPostDtos(myBulletinPostDtos)
                 .myAmenityDtos(myAmenityDtos)
@@ -55,7 +63,29 @@ public interface MemberMapper {
     }
 
     @Named("MTMLR")
-    MemberListResponseDto memberToMemberListResponseDto(Member member);
+    default MemberListResponseDto memberToMemberListResponseDto(Member member) {
+        if (member == null) {
+            return null;
+        }
+
+        /**
+         * 연관 관계 매핑은 memberId를 중심으로 연결되므로 followerNumber를 count할 경우
+         * 본인을 follow하는 사람들의 수가 아니라 본인이 follower로서 따르는 회원의 수가 된다.
+         * 그러므로 MemberMapper에서는 follower와 followee의 수를 반대로 입력해야 한다.
+         */
+        Long numberOfUserAsFollower = (long) member.getFollowers().size();
+        Long numberOfUserAsFollowee = (long) member.getFollowees().size();
+
+        MemberListResponseDto memberListResponseDto = MemberListResponseDto.builder()
+                .nickname(member.getNickname())
+                .dogName(member.getDogName())
+                .followerNumber(numberOfUserAsFollowee)
+                .followeeNumber(numberOfUserAsFollower)
+                .profileUrl(member.getProfileUrl())
+                .build();
+
+        return memberListResponseDto;
+    }
 
     @IterableMapping(qualifiedByName = "MTMLR")
     List<MemberListResponseDto> membersToMemberListResponseDtos(List<Member> members);
