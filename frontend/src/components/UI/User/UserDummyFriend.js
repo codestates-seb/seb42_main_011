73511d106608b7372681530844ac9b005651d127
wangamy0222 <wangamyy222@gmail.com>
2023-03-16 13:50:43 +0900

    [추가] user 관련 더미들 추가

--- frontend/src/components/UI/User/UserDummyFriend.js
+++ frontend/src/components/UI/User/UserDummyFriend.js
@@ -6,75 +6,80 @@ export default {
       message: '회원 정보입니다.',
       data: {
         dataid: 0,
-        nickname: 'maru_maru',
-        dogName: '마루',
-        dogGender: 'MALE',
+        nickname: 'cori_ana',
+        dogName: '코리',
+        dogGender: 'FEMALE',
         aboutMe:
-          '안녕하세요! 5살 남자 귀염둥이 마루예요! \n 저는 친구들 만나는거 정말 좋아해요! \n 친구들 많이 사귀어서 같이 산책하고 싶어요!\n 팔로우 대 환영입니다! :)',
-        followerNumber: 0,
-        followeeNumber: 10,
+          '안녕하세요! 저는 나름 귀족인 코리라고 해요! \n 먹는거 너무 좋아하고 놀기도 좋아하는 3살입니다 :) \n애견카페에서 같이 놀 친구들 구해요! \n 팔로우 해주시면 팔로우 해드려요!',
+        followerNumber: 2000000,
+        followeeNumber: 1500000,
         profileUrl:
-          'https://images.unsplash.com/photo-1554692936-82776f9406db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+          'https://images.unsplash.com/photo-1535930749574-1399327ce78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1336&q=80',
         myBulletinPostDtos: [
           {
             bulletinPostId: 1,
             photoUrl:
-              'https://images.unsplash.com/photo-1519098901909-b1553a1190af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2Vsc2glMjBjb3JnaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1626736637845-53045bb9695b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGdvbGRlbiUyMHJldHJpZXZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 2,
             photoUrl:
-              'https://images.unsplash.com/photo-1622065713075-a5e28dcc802c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8d2Vsc2glMjBjb3JnaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1556647034-7aa9a4ea7437?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGdvbGRlbiUyMHJldHJpZXZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 3,
             photoUrl:
-              'https://images.unsplash.com/photo-1589965716319-4a041b58fa8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1612774412771-005ed8e861d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z29sZGVuJTIwcmV0cmlldmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 4,
             photoUrl:
-              'https://images.unsplash.com/photo-1546975490-e8b92a360b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1637098063179-d73d8034621c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGdvbGRlbiUyMHJldHJpZXZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 5,
             photoUrl:
-              'https://images.unsplash.com/photo-1560743641-3914f2c45636?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1589756695365-4e0915b3a246?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzB8fGdvbGRlbiUyMHJldHJpZXZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 6,
             photoUrl:
-              'https://images.unsplash.com/photo-1546975490-a79abdd54533?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1624956741711-9e4001049662?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODB8fGdvbGRlbiUyMHJldHJpZXZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 7,
             photoUrl:
-              'https://images.unsplash.com/photo-1612160609504-334bdc6b70c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1618251425736-1502841bb3bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTB8fGdvbGRlbiUyMHJldHJpZXZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 8,
             photoUrl:
-              'https://images.unsplash.com/photo-1519098901909-b1553a1190af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2Vsc2glMjBjb3JnaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1508609540374-67d1601ba520?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA4fHxnb2xkZW4lMjByZXRyaWV2ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 9,
             photoUrl:
-              'https://images.unsplash.com/photo-1597633544424-4da83804df40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d2Vsc2glMjBjb3JnaXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1577936638249-39fa2e1d786a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAxfHxnb2xkZW4lMjByZXRyaWV2ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 10,
             photoUrl:
-              'https://images.unsplash.com/photo-1554692918-08fa0fdc9db3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1627323721367-94128c3fa0f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA1fHxnb2xkZW4lMjByZXRyaWV2ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 11,
             photoUrl:
-              'https://images.unsplash.com/photo-1554692901-e16f2046918a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1588496609626-5563093a510b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ5fHxnb2xkZW4lMjByZXRyaWV2ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
           },
           {
             bulletinPostId: 12,
             photoUrl:
-              'https://images.unsplash.com/photo-1561852184-92b9bb821045?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fHdlbHNoJTIwY29yZ2l8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+              'https://images.unsplash.com/photo-1606602059984-a97ea7617e33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjMyfHxnb2xkZW4lMjByZXRyaWV2ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
+          },
+          {
+            bulletinPostId: 13,
+            photoUrl:
+              'https://images.unsplash.com/photo-1616887446499-27116f0e3b05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjQ1fHxnb2xkZW4lMjByZXRyaWV2ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
           },
         ],
         myAmenityDtos: [
