a66ef1c6252cdccd5c12196f3e60c3fc728a03df
Jung Su <wjdwjdtn92@naver.com>
2023-03-16 12:11:54 +0900

    [추가] 친구 검색 결과 더미 추가

---
+++ frontend/src/components/Feeds/FRIEND_DUMY.js
@@ -0,0 +1,118 @@
+export default {
+  data: [
+    {
+      memberId: 1,
+      nickname: '코딩김',
+      dogName: '뽀삐',
+      followerNumber: 0,
+      followeeNumber: 10,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 2,
+      nickname: '박엑셀',
+      dogName: '뽀삐',
+      followerNumber: 10,
+      followeeNumber: 10,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 3,
+      nickname: '코딩김',
+      dogName: '뽀삐',
+      followerNumber: 0,
+      followeeNumber: 10,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 4,
+      nickname: '박엑셀',
+      dogName: '뽀삐',
+      followerNumber: 10,
+      followeeNumber: 0,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 5,
+      nickname: '코딩김',
+      dogName: '뽀삐',
+      followerNumber: 0,
+      followeeNumber: 10,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 6,
+      nickname: '박엑셀',
+      dogName: '뽀삐',
+      followerNumber: 10,
+      followeeNumber: 0,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 7,
+      nickname: '코딩김',
+      dogName: '뽀삐',
+      followerNumber: 0,
+      followeeNumber: 10,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 8,
+      nickname: '박엑셀',
+      dogName: '뽀삐',
+      followerNumber: 10,
+      followeeNumber: 0,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 9,
+      nickname: '코딩김',
+      dogName: '뽀삐',
+      followerNumber: 0,
+      followeeNumber: 10,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 10,
+      nickname: '박엑셀',
+      dogName: '뽀삐',
+      followerNumber: 10,
+      followeeNumber: 0,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 11,
+      nickname: '코딩김',
+      dogName: '뽀삐',
+      followerNumber: 0,
+      followeeNumber: 10,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+    {
+      memberId: 12,
+      nickname: '박엑셀',
+      dogName: '뽀삐',
+      followerNumber: 10,
+      followeeNumber: 0,
+      photoUrl:
+        'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    },
+  ],
+  pageInfo: {
+    page: 1,
+    size: 15,
+    totalElements: 2,
+    totalPages: 1,
+  },
+};
