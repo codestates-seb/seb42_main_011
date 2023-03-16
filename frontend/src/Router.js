15308b5173acc96f47e74c7229965e68d1485176
wangamy0222 <wangamyy222@gmail.com>
2023-03-16 13:50:00 +0900

    [수정] Layout 관련 페이지들 수정

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -4,12 +4,15 @@ import LoginPage from './Pages/LoginPage';
 import SignupPage from './Pages/SignupPage';
 import ErrorPage from './Pages/ErrorPage';
 import HomePage from './Pages/HomePage';
-import MyPage from './Pages/MyPage';
+import UserAboutmePage from './Pages/UserAboutmePage';
+import UserFeedPage from './Pages/UserFeedPage';
+import UserPlacePage from './Pages/UserPlacePage';
+import NonHeaderLayout from './components/Layout/NonHeaderLayout';
 
 function Router() {
   return (
     <Routes>
-      <Route element={<Layout displayBgimg />}>
+      <Route element={<NonHeaderLayout displayBgimg />}>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="*" element={<ErrorPage />} />
@@ -20,7 +23,12 @@ function Router() {
       </Route>
 
       <Route element={<Layout displayHeader displayProfile displayFlex />}>
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage" element={<UserAboutmePage />} />
+        <Route path="/mypage/feed" element={<UserFeedPage />} />
+        <Route path="/mypage/place" element={<UserPlacePage />} />
+        <Route path="/friendpage" element={<UserAboutmePage />} />
+        <Route path="/friendpage/feed" element={<UserFeedPage />} />
+        <Route path="/friendpage/place" element={<UserPlacePage />} />
       </Route>
     </Routes>
   );
