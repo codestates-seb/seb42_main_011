0d9f16190524548d3f7f58533459910d6f7746d1
Daw-Jeong <lvdst06@gmail.com>
2023-03-18 01:07:43 +0900

    [수정] like  수정 (2023.03.18 정다운)

--- backend/src/main/java/com/mybuddy/like/mapper/LikeMapper.java
+++ backend/src/main/java/com/mybuddy/like/mapper/LikeMapper.java
@@ -3,6 +3,7 @@ package com.mybuddy.like.mapper;
 import com.mybuddy.bulletin_post.service.BulletinPostService;
 import com.mybuddy.like.dto.LikeResponseDto;
 import com.mybuddy.like.entity.Like;
+import com.mybuddy.like.service.LikeService;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
@@ -11,10 +12,10 @@ public interface LikeMapper {
 
     //사실 매퍼이기보단 그냥 메서드 인데.. 일단 responseDto로 바꾸는 거긴 하니까 여기에?
     //인터페이스일 필요도 없긴 하지만 나중에 다른 기능이 필요할 수 있으니 매퍼로 일단!
-    default LikeResponseDto toLikeResponseDto(long postId, BulletinPostService bulletinPostService) {
+    default LikeResponseDto toLikeResponseDto(long postId, LikeService likeService) {
 
         LikeResponseDto likeResponseDto = new LikeResponseDto();
-        likeResponseDto.setLikeCount(bulletinPostService.getLikeCount(postId));
+        likeResponseDto.setLikeCount(likeService.getLikeCount(postId));
 
         return likeResponseDto;
     };
