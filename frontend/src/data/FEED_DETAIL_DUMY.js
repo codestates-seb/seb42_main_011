cf395f7176db964dfdd26568ef0e1d37f8e4fec5
Jung Su <wjdwjdtn92@naver.com>
2023-03-20 10:13:08 +0900

    [추가] 게시글 상세 더미 추가

---
+++ frontend/src/data/FEED_DETAIL_DUMY.js
@@ -0,0 +1,132 @@
+export default {
+  code: '200',
+  message: '게시물 1개 조회',
+  data: {
+    bulletinPostId: 1,
+    photoUrl:
+      'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    postContent:
+      '오늘의 기분! 너무너무 행복하다 :) \n 간식도 많이 먹고 산책도 나가고! 견생 최고의 날! 헤헤 그나저나 나 어때? 나 오늘 예쁘지? 멋있지? 귀엽지?! 오늘 오빠가 나 데리고 사진 찍으러 갔어! \n\n 루미 스튜디오에서 나 예쁘게 찍어주셨어! \n 멍친구들도 여기 가서 사진 예쁘게 찍어봐!',
+    createdAt: '2023-03-14T17:38:08.34353',
+    memberId: 1,
+    nickname: '알파벳',
+    dogName: '더닝크루거',
+    profileUrl:
+      'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    amenityId: 1,
+    amenityName: '카페 가고싶다',
+    likeCount: 1,
+    likeByUser: 2,
+    commentList: [
+      {
+        commentId: 1,
+        commentContent: '댓글댓글~',
+        memberId: 1,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 2,
+        commentContent: '댓글댓글~',
+        memberId: 2,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 3,
+        commentContent: '댓글댓글~',
+        memberId: 1,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 4,
+        commentContent: '댓글댓글~',
+        memberId: 2,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 5,
+        commentContent: '댓글댓글~',
+        memberId: 1,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 6,
+        commentContent: '댓글댓글~',
+        memberId: 2,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 7,
+        commentContent: '댓글댓글~',
+        memberId: 1,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 8,
+        commentContent: '댓글댓글~',
+        memberId: 2,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 9,
+        commentContent: '댓글댓글~',
+        memberId: 1,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 10,
+        commentContent: '댓글댓글~',
+        memberId: 7,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 11,
+        commentContent: '댓글댓글~',
+        memberId: 1,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        commentId: 12,
+        commentContent: '댓글댓글~',
+        memberId: 2,
+        nickName: '알파벳',
+        dogName: '더닝크루거',
+        profileUrl:
+          'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+    ],
+    commentCount: 12,
+  },
+};
