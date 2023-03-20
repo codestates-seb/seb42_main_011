22e13ebbfe2696279eaf4a9483a275529720bb2c
sdoaolo <09jieun01@naver.com>
2023-03-20 10:32:19 +0900

    [수정] exception code 정리

--- backend/src/main/java/com/mybuddy/global/exception/LogicExceptionCode.java
+++ backend/src/main/java/com/mybuddy/global/exception/LogicExceptionCode.java
@@ -4,14 +4,26 @@ import lombok.Getter;
 
 public enum LogicExceptionCode {
 
-    MEMBER_NOT_FOUND(404, "Member not found"),
-    MEMBER_ALREADY_EXISTS(409, "Member already exists"),
-    NICKNAME_ALREADY_EXISTS(409, "Nickname already exists"),
+    // 401 unauthorized : 클라이언트 요청이 전달되지 않음 (인증 부족- token)
     MEMBER_UNAUTHORIZED(401, "Member Unauthorized"),
+
+
+    // 403 forbidden : 서버에 요청 전달되었지만, 권한이 없음
+    FOLLOW_NOT_POSSIBLE(403, "You cannot follow yourself"),
+    NOT_RESOURCE_OWNER(403, "You are not a resource owner"),
+
+
+    //404 not found
+    MEMBER_NOT_FOUND(404, "Member not found"),
     BULLETIN_POST_NOT_FOUND(404, "Post not found"),
     COMMENT_NOT_FOUND(404, "Comment not found"),
-    FOLLOW_NOT_POSSIBLE(403, "You cannot follow yourself"),
+    AMENITY_NOT_FOUND(404,"Amenity not found"),
     FOLLOW_NOT_FOUND(404, "Follow data not found"),
+
+
+    // 409 conflict : 서버의 현재 상태와 요청이 충돌
+    MEMBER_ALREADY_EXISTS(409, "Member already exists"),
+    NICKNAME_ALREADY_EXISTS(409, "Nickname already exists"),
     FOLLOW_ALREADY_EXISTS(409, "Follow data already exists");
 
 
