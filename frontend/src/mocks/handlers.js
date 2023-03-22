52decb4c47b8a54803a16b4386057748c67a3408
Jung Su <wjdwjdtn92@naver.com>
2023-03-22 14:56:28 +0900

    [추가] 무한스크롤 테스트를 위한 mock 추가

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -7,6 +7,8 @@ import {
   getMember,
   pathchMember,
   deleteMember,
+  getPosts,
+  getSearchs,
 } from './resolvers/member.resolvers';
 
 export default [
@@ -14,6 +16,8 @@ export default [
   rest.post('/api/v1/auth/logout', postLogout),
   rest.post('/api/v1/members', postSignup),
   rest.get('/api/v1/members/:memberId', getMember),
+  rest.get('/api/v1/bulletin-posts', getPosts),
+  rest.get('/api/v1/search', getSearchs),
   rest.patch('/api/v1/members/:memberId', pathchMember),
   rest.delete('/api/v1/members', deleteMember),
 ];
