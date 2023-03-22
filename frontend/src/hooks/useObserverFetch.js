d1aa994ed67e608c775748310f37a841f104aa3f
Jung Su <wjdwjdtn92@naver.com>
2023-03-22 14:51:01 +0900

    [추가] 무한스크롤 hooks 개발

---
+++ frontend/src/hooks/useObserverFetch.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+import useIntersectionObserver from './useIntersectionObserver';
+
+function useObserverFetch({ onFetch, rootMargin = '100px' }) {
+  const ref = useRef(null); // 감시할 엘리먼트
+  const entry = useIntersectionObserver({
+    rootMargin,
+    targetRef: ref,
+  });
+
+  const isIntersecting = !!entry?.isIntersecting; // 겹치는 영역이 존재하는 지 여부
+
+  useEffect(() => {
+    if (isIntersecting) onFetch(); // 목록의 마지막에 도달했을 때, 리스트를 더 불러오도록 요청한다.
+  }, [isIntersecting]);
+
+  return { ref };
+}
+
+export default useObserverFetch;
