c393644548ba04c04e91ceaa95ec664b4faff284
sdoaolo <09jieun01@naver.com>
2023-03-24 16:16:44 +0900

    [추가] 비밀번호 재설정 관련 - 토큰 검증 - 엔드포인트 추가 등

--- backend/src/main/java/com/mybuddy/global/config/InterceptorConfig.java
+++ backend/src/main/java/com/mybuddy/global/config/InterceptorConfig.java
@@ -10,7 +10,5 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginUserInterceptor());
-        //comments는 전체 엔드포인트에 해당해서 인터셉터를 추가해도 되어서 여기다 이런식으로 작성해도 되지만 ->  .addPathPatterns("/api/v1/comments");
-        //일단 주석처리하고, LoginUserInterceptor() 내부에서 처리되도록 작성해 놓았습니다. (2023.03.17 강지은)
     }
 }
