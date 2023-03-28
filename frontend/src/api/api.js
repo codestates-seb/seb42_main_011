0d38cd348f135bf9611c01cc36563fde27a375c1
Jung Su <wjdwjdtn92@naver.com>
2023-03-28 12:17:25 +0900

    [추가] 헤더 cors 옵션 추가

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -6,6 +6,8 @@ const api = axios.create({
   baseURL: BASE_URL,
   headers: {
     withCredentials: true,
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Origin': 'https://my-buddy.co.kr',
   },
 });
 
