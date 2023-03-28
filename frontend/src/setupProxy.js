d4b9e6bac3f6509f67a3297b037999423a4f7cfb
Jung Su <wjdwjdtn92@naver.com>
2023-03-28 10:12:06 +0900

    [수정] 타겟 도메인 수정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     '/api/v1', // proxy가 필요한 path prameter를 입력합니다.
     createProxyMiddleware({
       target:
-        'http://ec2-43-200-164-30.ap-northeast-2.compute.amazonaws.com:8081', // 타겟이 되는 api url를 입력합니다.
+        'https://mybuddy-loadbalancer-369953592.ap-northeast-2.elb.amazonaws.com', // 타겟이 되는 api url를 입력합니다.
       changeOrigin: true, // 대상 서버 구성에 따라 호스트 헤더가 변경되도록 설정하는 부분입니다.
     }),
   );
