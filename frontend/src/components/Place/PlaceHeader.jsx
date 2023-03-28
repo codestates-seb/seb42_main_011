861e98c69c2f232ba4b88c2a2fc82c81fe822d82
wangamy0222 <wangamyy222@gmail.com>
2023-03-28 11:56:38 +0900

    [추가, 수정] amenity page 생성/ api, router, 관련 페이지들 수정

--- frontend/src/components/Place/PlaceHeader.jsx
+++ frontend/src/components/Place/PlaceHeader.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import PostHeader from '../UI/PostHeader';
 import PlaceLogo from '../../assets/logo/place_logo.svg';
 
-function PlaceHeader() {
-  return (
-    <PostHeader
-      title="home"
-      img={PlaceLogo}
-      description="친구들이 많이 방문한 장소예요!"
-    />
-  );
+function PlaceHeader({ amenityName }) {
+  const location = useLocation();
+  const isAmenityPage = location.pathname.includes('/amenity');
+
+  const description = isAmenityPage
+    ? `${amenityName}에 방문한 멍친구들의 게시글이에요!`
+    : '친구들이 많이 방문한 장소예요!';
+  return <PostHeader title="home" img={PlaceLogo} description={description} />;
 }
 
 export default PlaceHeader;
