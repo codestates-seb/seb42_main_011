15b72c70d8424cc193b22096a908b0fae2460f55
wangamy0222 <wangamyy222@gmail.com>
2023-03-29 00:03:40 +0900

    [수정] 회원정보 관련 페이지들 수정

--- frontend/src/Pages/UserPlacePage.jsx
+++ frontend/src/Pages/UserPlacePage.jsx
@@ -88,14 +88,13 @@ const NoDataText = styled.p`
   line-height: 3rem;
   font-size: var(--font-size-20);
   font-weight: 500;
-  margin-top: 20px;
+  margin-top: 0px;
 `;
 
-function UserPlacePage({ userdata, isMypage }) {
+function UserPlacePage({ userdata, isMyPage }) {
   const navigate = useNavigate();
 
   const handlePlaceClick = (amenityId, amenityName) => {
-    console.log('amenityId:', amenityId, 'amenityName:', amenityName);
     navigate(`/amenity/${amenityId}`, { state: { amenityName } });
   };
 
@@ -127,14 +126,16 @@ function UserPlacePage({ userdata, isMypage }) {
           )
         ) : (
           <TextWrapper>
-            {isMypage ? (
+            {isMyPage ? (
               <NoDataText>
                 아직 추천한 장소가 없어요!
-                <br />
-                위치를 추가해 장소를 추천해보세요 :)
+                <br />글 작성시 위치를 추가해 장소를 추천해보세요 :)
               </NoDataText>
             ) : (
-              <NoDataText>아직 친구가 추천한 장소가 없어요! :)</NoDataText>
+              <NoDataText>
+                <br />
+                아직 친구가 추천한 장소가 없어요! :)
+              </NoDataText>
             )}
           </TextWrapper>
         )}
