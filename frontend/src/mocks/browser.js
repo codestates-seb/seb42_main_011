32cf3e69381a52f1f6d9d282b609af551cdb3b9b
Jung Su <wjdwjdtn92@naver.com>
2023-03-31 02:40:53 +0900

    [추가] 게시글 상세 테스트 개발을 위해 mock 수정

--- frontend/src/mocks/browser.js
+++ frontend/src/mocks/browser.js
@@ -1,8 +1,8 @@
 // src/mocks/browser.js
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { setupWorker } from 'msw';
-// import handlers from './handlers';
-import { logout, login, register } from '../redux/services/auth.service';
+import handlers from './handlers';
+// import { logout, login, register } from '../redux/services/auth.service';
 // This configures a Service Worker with the given request handlers.
-// export default setupWorker(...handlers);
-export default setupWorker(logout, login, register);
+export default setupWorker(...handlers);
+// export default setupWorker(logout, login, register);
